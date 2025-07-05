b4b272f35f793a87b491ca929ea500b17af6374d
Asik36 <102484705+Asik36@users.noreply.github.com>
2025-07-05 23:43:56 +0300

    NUMBERS done

--- week1/NUMBERS/NUMBERS.c
+++ week1/NUMBERS/NUMBERS.c
@@ -3,201 +3,154 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define NUM_OF_UNIQUE_NUMBER_WORDS 28
-#define UPPER_LIMIT 99
-#define MAX_DIGITS 2
-
-int covert(int argc, char **argv);
-int fileInput(char *input_file,char * input);
-int fileOutput(char **output_file,char * output);
-void getUserInput(char *input);
-void getUserInput(char *input);
-void printOutput(char *output[100]);
-int translateFromStringToInt(char str_numbers[100],int numbers[100]);
-char * getLingusticNumber(int num,char str_number[20]);
-void numberWordBuilder(int num,int lingustic_num[MAX_DIGITS]);
-
+/*Array of each uniqe name for a number in english from 0 - 99*/
 static const char *number_names[NUM_OF_UNIQUE_NUMBER_WORDS] = {
-   [0]="zero",
-   [1]="one",
-   [2]="two",
-   [3]="three",
-   [4]="four",
-   [5]="five",
-   [6]="six",
-   [7]="seven",
-   [8]="eight",
-   [9]="nine",
-   [10]="ten",
-   [11]="eleven",
-   [12]="twelve",
-   [13]="thirteen",
-   [14]="fourteen",
-   [15]="fifteen",
-   [16]="sixteen",
-   [17]="seventeen",
-   [18]="eighteen",
-   [19]="nineteen",
-   [20]="twenty",
-   [21]="thirty",
-   [22]="forty",
-   [23]="fifty",
-   [24]="sixty",
-   [25]="seventy",
-   [26]="eighty",
-   [27]="ninety"
+   "one",
+   "zero",
+   "two",
+   "three",
+   "four",
+   "five",
+   "six",
+   "seven",
+   "eight",
+   "nine",
+   "ten",
+   "eleven",
+   "twelve",
+   "thirteen",
+   "fourteen",
+   "fifteen",
+   "sixteen",
+   "seventeen",
+   "eighteen",
+   "nineteen",
+   "twenty",
+   "thirty",
+   "forty",
+   "fifty",
+   "sixty",
+   "seventy",
+   "eighty",
+   "ninety"
 };
+void numberToWord(int num,char * word);
+void readFromInputFile(char * path);
+void printNumbers(char * line);
+void readFromUser(void);
+void numbers(int argc, char **argv);
 int main(int argc, char **argv)
-{   
-    covert(argc,argv);
-    printf("\n");
+{ 
+    numbers(argc, argv);
     return 0;
 }
-int covert(int argc, char **argv){
-    char input[100];
-    char * output[100];
-    int * numbers;
-    int numbers_count;
-    if(1){ //               TODO: get input from files
-        getUserInput(input);
-    }
-    numbers_count = translateFromStringToInt(input,numbers);
-    printf("number count : %d\n",numbers_count);
-    for (int i = 0; i < numbers_count; i++)
+/** 
+ * numbers - starts the program
+ */
+void numbers(int argc, char **argv)
+{
+    if(argc > 1) /*read file*/ 
     {
-        getLingusticNumber(numbers[i],output[i]);
+        readFromInputFile(argv[1]);
+    }else{
+        readFromUser();
     }
 
-    if(1){      // TODO: give output to files
-        printOutput(output);
-    }
-    
-    
-
-
-
 
 }
-int fileInput(char *input_file,char * input)
+
+
+/**
+ * readFromInputFile - gets numbers from file
+ * @path - file path
+ * 
+ */
+void readFromInputFile(char * path)
 {
-    FILE *fptr;
-    if(fptr = fopen(input_file, "r") == NULL){
-        return 0;
-    }
-    fgets(input, 100, fptr); 
-    fclose(fptr);
-    return 0;
-}
-int fileOutput(char **output_file,char * output)
-{
-    FILE *fptr;
-    if(fptr = fopen(output_file, "w") != NULL){
-        return 1;        
-    }
-    // Write some text to the file
-    fprintf(fptr, output); 
-    fclose(fptr);
-    return 0;
-}
-void getUserInput(char *input){
-    fgets(input,sizeof(char)*100,stdin);
-}
-void printOutput(char *output[100])
-{
-    int i=0;
-    while(output[i])
-    {
-        printf("%s\n",output[i]);
-        i++;
-    }
     
+    char line[100];
 
-}
-int translateFromStringToInt(char str_numbers[100],int numbers[100])
-{
-    int count = 0;
-    char * token = strtok(str_numbers, " ");
-
-     while( token != NULL ) {
-      numbers[count++] = atoi(token);
-      token = strtok(NULL, " ");
-   }
-    return count;
-    
-    
-}
-char * getLingusticNumber(int num, char str_number[20])
-{
-    int isZero = 1;
-    str_number = "";
-    int lingustic_num[MAX_DIGITS]; 
-    
-    numberWordBuilder(num, lingustic_num);
-    for (int i = 0; i < MAX_DIGITS && isZero; i++)
-    {
-        if(lingustic_num[i] !=0)
-        {
-            isZero = 0;
+    FILE *fptr = (FILE*)fopen(path, "r");
+        if(fptr== NULL){
+            printf("Could not find the file!\n");
         }
-    }
-    if(!isZero){
-        for (int i = 0; i < MAX_DIGITS; i++)
+        else
         {
-            if(lingustic_num[i]!=0)
+            while(fgets(line,sizeof(line),fptr) != NULL)
             {
-               // strcat(str_number ,number_names[lingustic_num[i]]);
-                printf("%s ",number_names[lingustic_num[i]]);
+                printNumbers(line);
             }
+            fclose(fptr);
         }
 
+}
+/**
+ * readFromUser - ask user input from command line
+ */
+void readFromUser(void)
+{
+    char line[100];
+    fgets(line,sizeof(line),stdin);
+    printNumbers(line);  
+}
+
+/**
+ * printNumbers - prints a line of numbers as words.
+ * @line - string of numbers from 1-99
+ */
+void printNumbers(char * line)
+{
+    char *start = line;
+    char * next = start;
+    char number_word[30];
+    strcat(line, " ");
+
+    while(*next != '\0')
+    {
+        if(*next == ' ' || *next == '\t' || *next =='\0') /*read while space or tab or end of line*/
+        {
+            *next = '\0';
+            numberToWord(atoi(start),number_word); /*number to word*/ 
+            printf("%s \n",number_word); 
+            start = ++next; /* go to the next number*/
+
+        }
+        else
+        {
+            next++;
+        }
+    }
+}
+/**
+ * numberToWord - Convert an Integer into a string using @number_names
+ * @num         - Sourse number
+ * @word        - Output number as a string
+ */
+void numberToWord(int num,char * word)
+{
+    
+    /*Word equals zero only when its the only digit in the number*/
+    if(num == 0) 
+    {
+        strcpy(word,number_names[num]);
     }
     else
     {
-        strcpy(str_number,number_names[0]);
-    }
-            printf("\n");
-
-    return str_number;
-
-
-}
-void numberWordBuilder(int num,int lingustic_num[MAX_DIGITS])
-{
-        int index = 0;
-        if(num<20){
-            lingustic_num[index]   = 0;
-            lingustic_num[index+1] = num;
-        }else{
-            lingustic_num[index] = 18+num/10;
-            lingustic_num[index+1] = num%10;
+        /*If the number between 1-20 the number values corresponds to the correct index in number_names */
+        if(num<20) 
+        {
+            strcpy(word,number_names[num]);
         }
-}    
-
-
-
-
-
-
-
-// void numberWordBuilder(int num,int lingustic_num[MAX_DIGITS],int index)
-// {
-//     if(index == MAX_DIGITS)
-//     {
-//         return;
-//     }
-//     if(num<20)
-//     {
-//         lingustic_num[index] = num;
-
-//     }
-//     else
-//     {
-//         lingustic_num[index] = 18+num/10;
-
-//     }
-//     numberWordBuilder(num/10,lingustic_num,index+1);
-
-// }
-
-
-// 0 - 3
-// 0 - 10
+        else /*If the number is between is greater then 20*/
+        {
+            strcpy(word,number_names[18 + num/10]); /*The index of the first word (tens) equals to value of the digit +18(for skipping the numbers between 1-20)*/
+            strcat(word , " "); /*Space between the words*/
+            if(num%10 != 0)
+            {
+                strcat(word, number_names[num % 10]); /*Get the world for the remining digit*/
+            }
+        }
+    }
+    
+    
+}
