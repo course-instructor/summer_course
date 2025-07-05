5e771817535e245d150c1a8142e50a4a1223eaba
tomersharabi <138164137+tomersharabi@users.noreply.github.com>
2025-07-05 20:04:22 +0300

    Update CON.c
    tomer comments

--- week1/CON/CON.c
+++ week1/CON/CON.c
@@ -10,18 +10,22 @@
  *  input The original string.
  *  updating the requested array
  */
+
 void contract(const char input[], char output[])
 {
+///Tomer comment: not vary clear for variable names i would use index_read/index_write
     int input_index = 0;
     int output_index = 0;
-
+///Tomer comment: "input[input_index + 2]- input[input_index + 1]" if the input is not enough characters you risk a crash here and access not within the array. 
     while (input[input_index] != '\0')// while we are not in the end
     {
         if (input[input_index + 1] - input[input_index] == ASCII_DIFF &&
             input[input_index + 2] - input[input_index + 1] == ASCII_DIFF)//if there is a sequence of 3 values
         {
             output[output_index++] = input[input_index++];
+/// Tomer comment: It's not clear enough what the function does. 
             while (input[input_index + 1] - input[input_index] == ASCII_DIFF)
+///Tomer comment: i know you dont have to use '{}' when you write only one line but its better for other people for reading.
                 input_index++;//moving until the sequence is over
             output[output_index++] = '-';
             output[output_index++] = input[input_index++];//copying the final char of the sequence
@@ -49,7 +53,7 @@ int main()
 
     printf("Please enter your sequence:\n");
     fgets(input, MAX_LEN, stdin);
-
+/// Tomer comment: How do you know if the function is succeded or failed?.
     contract(input, shorted);
 
     printf("The original input was:\n%s\n", input);
