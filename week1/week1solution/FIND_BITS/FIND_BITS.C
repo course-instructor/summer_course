1b1e4de84771a8f920919cc87f599f3d7c9a5122
tomersharabi <138164137+tomersharabi@users.noreply.github.com>
2025-07-05 19:07:59 +0300

    Update FIND_BITS.C
    tomer comments

--- week1/week1solution/FIND_BITS/FIND_BITS.C
+++ week1/week1solution/FIND_BITS/FIND_BITS.C
@@ -17,6 +17,7 @@ void printinput(unsigned long input)
 	}
 	printf("\n");
 }
+///tomer comments: in our team we use retvals in functions, please use it also. tell me if you need more explantion.
 void main()
 {
 	unsigned long x;
@@ -25,9 +26,13 @@ void main()
 	scanf_s("%lu", &x);
 	printinput(x);
 	printf("\n please enter a value for y: ");
+///tomer comments: why you used scanf_s? scanf_s is not supporded in linux so you you probably didn't compile with gcc.
 	scanf_s("%lu", &y);
 	printinput(y);
+///tomer comments: this variable can be confusing i would call it "mask" or something else that other people will understand, its a standart in our team.
+/// also you used "(sizeof(unsigned long) * 8)" many times, its better to defined as a macro.
 	unsigned long check = 1UL << (sizeof(unsigned long) * 8 - 1);
+
 	int counter = 0;
 	for (int i = 0;  i < sizeof(unsigned long)*8;  i++)
 	{
