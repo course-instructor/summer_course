9395f63236084614beb19d7e8c350579c976b608
Alon Rod <alonrod848@gmail.com>
2025-07-06 12:53:34 +0300

    find bits fixed

--- week1/week1solution/FIND_BITS/FIND_BITS.C
+++ week1/week1solution/FIND_BITS/FIND_BITS.C
@@ -1,46 +1,85 @@
 #include <stdio.h>
-void printinput(unsigned long input) 
+
+#define BITS sizeof(unsigned long) * 8
+
+int mask(unsigned long input) 
 {
-	unsigned long bit = 1UL << (sizeof(unsigned long) * 8 - 1);
-	printf("\n");
-	for (int i = 0; i < sizeof(unsigned long) * 8; i++)
-	{
-		if (input & bit)
-		{
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
-		bit >>= 1;
-	}
-	printf("\n");
+    int retval = 0;
+
+    if (BITS > (sizeof(unsigned long) * 8))
+    {
+        printf("BITS too large!\n");
+        retval = 1;
+    }
+    else
+    {
+        unsigned long bit = 1UL << (BITS - 1);
+
+        printf("\n");
+        for (int i = 0; i < BITS; i++)
+        {
+            if (input & bit)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+            bit >>= 1;
+        }
+        printf("\n");
+    }
+
+    return retval;
 }
-void main()
+
+int main(void)
 {
-	unsigned long x;
-	unsigned long y;
-	printf("please enter a value for x: ");
-	scanf_s("%lu", &x);
-	printinput(x);
-	printf("\n please enter a value for y: ");
-	scanf_s("%lu", &y);
-	printinput(y);
-	unsigned long check = 1UL << (sizeof(unsigned long) * 8 - 1);
-	int counter = 0;
-	for (int i = 0;  i < sizeof(unsigned long)*8;  i++)
-	{
-		if ((x & check) && (y & check))
-		{
-			counter++;
-			printf("1");
-		}
-		else
-		{
-			printf("-");
-		}
-		check >>= 1;
-	}
-	printf("\n The amount of bits in same place that are on is: %d", counter);
+    int retval = 0;
+    unsigned long x;
+    unsigned long y;
+
+    printf("please enter a value for x: ");
+    if (scanf("%lu", &x) != 1)
+    {
+        printf("cant read x\n");
+        retval = 1;
+    }
+    else
+    {
+        mask(x);
+
+        printf("\nplease enter a value for y: ");
+        if (scanf("%lu", &y) != 1)
+        {
+            printf("cant read y\n");
+            retval = 1;
+        }
+        else
+        {
+            mask(y);
+
+            unsigned long check = 1UL << (BITS - 1);
+            int counter = 0;
+
+            for (int i = 0; i < BITS; i++)
+            {
+                if ((x & check) && (y & check))
+                {
+                    counter++;
+                    printf("1");
+                }
+                else
+                {
+                    printf("-");
+                }
+                check >>= 1;
+            }
+
+            printf("\nThe amount of bits in same place that are on is: %d\n", counter);
+        }
+    }
+
+    return retval;
 }
